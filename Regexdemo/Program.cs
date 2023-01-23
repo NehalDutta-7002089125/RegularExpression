2e5e45c4cc84ed09dd83b780de087d164c9e8811
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-23 16:59:23 +0530

    [Nehal] Entering valid user id

---
+++ Regexdemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Regexdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RGX regex= new RGX();
+            Console.WriteLine( regex.b("Sam"));
+        }
+    }
+}
---
+++ Regexdemo/RGX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regexdemo
+{
+    internal class RGX
+    {
+        public static string a = "^[a-zA-Z]{3}$";
+
+        public bool b(string c)
+        {
+            return Regex.IsMatch(c,a);
+        }
+    }
+}
